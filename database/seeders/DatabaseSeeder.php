0c8d926d2e0190929763ed36d495452e555cc98f
Nguyễn Thành Tuấn <133647354+thanhtuanxzx@users.noreply.github.com>
2024-12-10 15:14:37 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       $this->call([
-            NguoiDungSeeder::class,
-            XeMaySeeder::class,
-    }
+          NguoiDungSeeder::class,
+          XeMaySeeder::class,
+      ]);
+  }
 }
